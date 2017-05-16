6f0065419a0cd5035f56ba462f3a879f8ca4c86d
mickey3cutter <mickey3cutter@gmail.com>
2017-05-16 20:58:57 +0300

    updated about

--- about.php
+++ about.php
@@ -35,11 +35,23 @@ get_header(); ?>
 	<?php // Output custom post type in loop
 	$the_query = new WP_Query( 'post_type=portfolio&taxonomy=home_feat' );
 	if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); 
-	$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+	$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+	$terms = wp_get_object_terms($post->ID, 'projects_category');
+	?>
 		<img src="<?php echo $url; ?>">
+		<div class="category"><?php echo $terms[0]->name; ?>
 	<?php endwhile; endif; wp_reset_postdata(); ?>
 
 </div>
+
+<!-- Show a list of cpt taxonomis -->
+<ul class="list-inline isoto-filter text-center">
+	<li class="active"><a href="#">All</a></li>
+	<?php $terms = get_terms( 'projects_category', array( 'hide_empty' => false) ); 
+	foreach ($terms  as $term ) { ?>
+	<li><a href="#" data-filter=".<?php echo $term->slug; ?>"><?php echo $term->name; ?></a></li>
+	<?php } ?>
+</ul>
 
 
 <?php get_footer(); ?>
